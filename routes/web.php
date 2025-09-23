74a685234c2c849c7351e574b86f54b8e0e94efd
mitsyara <mitsyara@gmail.com>
2025-09-23 13:39:44 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Filament\Facades\Filament;
 
-Route::view('/', 'welcome')->name('home');
+// Route::view('/', 'welcome')->name('home');
+
+Route::get('/', function () {
+    $panel = Filament::getCurrentOrDefaultPanel();
+    return redirect($panel->getLoginUrl(['tenant' => $panel]));
+});
